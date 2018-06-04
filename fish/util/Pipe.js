59a02f753a5d823a9fc1e5b16a899842dc78bf02
Samuel Clarke <samuel.clarke@mindera.com>
2018-06-04 19:41:23 +0100

    Updated Pipe to be class and not prototype

--- fish/util/Pipe.js
+++ fish/util/Pipe.js
@@ -1,98 +1,99 @@
 
 define(function(){
-	
-	function Pipe(f, headPipe){
+	class Pipe{
+	  constructor(f, headPipe){
 		
-		this.f = f ? f : (v)=>v;
-		if(headPipe) this.headPipe = headPipe;
+		  this.f = f ? f : (v)=>v;
+		  if(headPipe) this.headPipe = headPipe;
 		
-		this.watchHash = {};
-		this.watchCount = 0;
+		  this.watchHash = {};
+		  this.watchCount = 0;
 
-		this.childPipeList = [];
-		this.isHead = false;
-	}
-	
-	Pipe.prototype.Head = function(){
+		  this.childPipeList = [];
+		  this.isHead = false;
+	  }
+    
+    Head(){
 
-		if(this.headPipe) throw 'Pipe already has head';
+      if(this.headPipe) throw 'Pipe already has head';
 
-		this.isHead = true;
-		this.namePipeList = [];
+      this.isHead = true;
+      this.namePipeList = [];
 
-		return this;
-	}
+      return this;
+    }
 
-	Pipe.prototype.push = function(v){
-		
-		v = this.f(v);
-		if(v === null) return;
-		Object.keys(this.watchHash).map(key => this.watchHash[key]).map(f => f(v));
-		this.childPipeList.map(pipe => pipe.push(v));
+    push(v){
+      
+      v = this.f(v);
+      if(v === null) return;
+      Object.keys(this.watchHash).map(key => this.watchHash[key]).map(f => f(v));
+      this.childPipeList.map(pipe => pipe.push(v));
 
-		return this;
-	};
+      return this;
+    };
 
-	Pipe.prototype.pushOnly = function(){
-		return (v)=> this.push(v);
-	}
-	
-	Pipe.prototype.watch = function(f){
-		
-		let id = this.watchCount++;
-		this.watchHash[id] = f;
-		
-		return () => delete this.watchHash[id];
-	}
+    pushOnly(){
+      return (v)=> this.push(v);
+    }
+    
+    watch(f){
+      
+      let id = this.watchCount++;
+      this.watchHash[id] = f;
+      
+      return () => delete this.watchHash[id];
+    }
 
-	Pipe.prototype.map = function(f){
-		let pipe = new Pipe(f, this.isHead ? this : this.headPipe);
-		this.childPipeList.push(pipe);
-		return pipe;
-	}
+    map(f){
+      let pipe = new Pipe(f, this.isHead ? this : this.headPipe);
+      this.childPipeList.push(pipe);
+      return pipe;
+    }
 
-	// pass a context set versio of the watch function
-	Pipe.prototype.watchOnly = function(){
-		return (f) => this.watch(f);
-	}
+    // pass a context set versio of the watch function
+    watchOnly(){
+      return (f) => this.watch(f);
+    }
 
-	Pipe.prototype.destroy = function(){
-		this.f = null;
-		this.watchHash = null;
-		this.watchCount = null;
-		this.childPipeList.forEach(pipe => pipe.destroy());
-		this.childPipeList = null;
-	}
+    destroy(){
+      this.f = null;
+      this.watchHash = null;
+      this.watchCount = null;
+      this.childPipeList.forEach(pipe => pipe.destroy());
+      this.childPipeList = null;
+    }
 
-	Pipe.prototype.getHead = function(){
-		let head = this.isHead ? this : this.headPipe;
+    getHead(){
+      let head = this.isHead ? this : this.headPipe;
 
-		if(!head) throw 'absent head';
+      if(!head) throw 'absent head';
 
-		return head;
-	}
+      return head;
+    }
 
-	Pipe.prototype.setName = function(name){
-		let head = this.getHead();
+    setName(name){
+      let head = this.getHead();
 
-		// check that name is not already taken
-		if(head.namePipeList.find(pipe => pipe.name === name)) throw 'name already taken';
+      // check that name is not already taken
+      if(head.namePipeList.find(pipe => pipe.name === name)) throw 'name already taken';
 
-		this.name = name;
-		if(!head.namePipeList.find(pipe => pipe === this)) head.namePipeList.push(this);
+      this.name = name;
+      if(!head.namePipeList.find(pipe => pipe === this)) head.namePipeList.push(this);
 
-		return this;
-	}
-	
-	Pipe.prototype.getName = function(name){
-    let pipe = this.getHead().namePipeList.find(pipe => pipe.name === name);
-    if(!pipe) throw `Unknown name ${name}`;
-    return pipe;
-	}
+      return this;
+    }
+    
+    getName(name){
+      let pipe = this.getHead().namePipeList.find(pipe => pipe.name === name);
+      if(!pipe) throw `Unknown name ${name}`;
+      return pipe;
+    }
 
-	Pipe.prototype.name = function(name){
-		return this.getName(name);
-	}
-
+    name(name){
+      return this.getName(name);
+    }
+  }
+  
 	return Pipe;
 });
