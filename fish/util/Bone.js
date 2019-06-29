d6e5b65292d2655462cda614643da3a306a48829
Samuel Clarke <samuel.clarke@mindera.com>
2019-06-29 01:07:59 +0100

    bone functions

--- fish/util/Bone.js
+++ fish/util/Bone.js
@@ -22,13 +22,35 @@ const buildBoneLine = (id, positionList, duration) => {
   );
 };
 
-const boneSoup = [
-  ...new Array(4).fill(null).map((v, i) => buildBone("click", i / 4, 1 / 8)),
+const renderBone = (input, bone, startTime, length) => {
+  input.push(bone(startTime, length));
+};
+
+const renderBroth = (input, broth, startTime, length) => {
+  broth.forEach(bone => renderBone(input, bone, startTime, length));
+};
+
+const renderSlosh = (input, slosh, startTime, length) => {
+  for (let i = 0; i < slosh.length; i++) {
+    const time = startTime * (length * i);
+    this.renderBroth(input, slosh[i], time, length);
+  }
+
+  return tartTime + length * slosh.length;
+};
+
+const broth = [
+  // ...new Array(4).fill(null).map((v, i) => buildBone("click", i / 4, 1 / 8)),
+  ...buildBoneLine("click", [1, 1, 1, 1], 1 / 8),
   buildBone("kick", 0, 1 / 8),
   buildBone("kick", 3 / 8),
   buildBone("snare", 2 / 4, 1 / 8)
 ];
 
 module.exports = {
-  buildBone
+  buildBone,
+  buildBoneLine,
+  renderBone,
+  renderBroth,
+  renderSlosh
 };
